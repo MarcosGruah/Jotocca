2e2c19cd4b6260dfda2a4375b75dbb2a2841b88a
MarcosGruah <110102063+MarcosGruah@users.noreply.github.com>
2024-01-26 23:54:51 -0300

    Added CategoryWithSubcategory View

---
+++ Jotocca.Database/dbo/Views/CategoryWithSubcategory.sql
@@ -0,0 +1,8 @@
+﻿CREATE VIEW [dbo].[CategoryWithSubcategory]
+AS SELECT
+    C.[Name] AS CategoryName,
+    S.[Name] AS SubcategoryName
+FROM
+    dbo.Category C
+INNER JOIN
+    dbo.Subcategory S ON C.[CategoryId] = S.[CategoryId];
