e6985647d465c56d0db0e7ebdcd12fc8f53dbd21
MarcosGruah <110102063+MarcosGruah@users.noreply.github.com>
2024-01-27 13:37:30 -0300

    Product seeding and new View
    Added a Post Deployment Database seeding for the Product table
    
    Added a View for the Product and their Subcategory

--- Jotocca.Database/dbo/Script.PostDeployment.sql
+++ Jotocca.Database/dbo/Script.PostDeployment.sql
@@ -64,3 +64,79 @@ BEGIN
 	INSERT INTO dbo.[Subcategory] ([CategoryId], [Name], [Description])
 	VALUES (@ApparelAndGearId, 'Boots', 'Protective boots');
 END
+
+IF NOT EXISTS (SELECT 1 FROM dbo.[Product])
+BEGIN
+	DECLARE @ScooterId UNIQUEIDENTIFIER = (SELECT [SubcategoryId] FROM dbo.Subcategory WHERE [Name] = 'Scooter');
+	DECLARE @StreetId UNIQUEIDENTIFIER = (SELECT [SubcategoryId] FROM dbo.Subcategory WHERE [Name] = 'Street');
+	DECLARE @TrailId UNIQUEIDENTIFIER = (SELECT [SubcategoryId] FROM dbo.Subcategory WHERE [Name] = 'Trail');
+	DECLARE @EngineId UNIQUEIDENTIFIER = (SELECT [SubcategoryId] FROM dbo.Subcategory WHERE [Name] = 'Engine');
+	DECLARE @ElectronicsId UNIQUEIDENTIFIER = (SELECT [SubcategoryId] FROM dbo.Subcategory WHERE [Name] = 'Electronics');
+	DECLARE @AccessoriesId UNIQUEIDENTIFIER = (SELECT [SubcategoryId] FROM dbo.Subcategory WHERE [Name] = 'Accessories');
+	DECLARE @CasualId UNIQUEIDENTIFIER = (SELECT [SubcategoryId] FROM dbo.Subcategory WHERE [Name] = 'Casual');
+	DECLARE @HelmetsId UNIQUEIDENTIFIER = (SELECT [SubcategoryId] FROM dbo.Subcategory WHERE [Name] = 'Helmets');
+	DECLARE @GlovesId UNIQUEIDENTIFIER = (SELECT [SubcategoryId] FROM dbo.Subcategory WHERE [Name] = 'Gloves');
+	DECLARE @BootsId UNIQUEIDENTIFIER = (SELECT [SubcategoryId] FROM dbo.Subcategory WHERE [Name] = 'Boots');
+
+	INSERT INTO dbo.[Product] ([SubcategoryId], [Name], [Description], [Price], [Quantity], [Image])
+	VALUES (@ScooterId, 'Scooter 1', 'Scooter 1 description', 10000.00, 10, 'images/scooter1.jpg');
+
+	INSERT INTO dbo.[Product] ([SubcategoryId], [Name], [Description], [Price], [Quantity], [Image])
+	VALUES (@ScooterId, 'Scooter 2', 'Scooter 2 description', 20000.00, 10, 'images/scooter2.jpg');
+
+    INSERT INTO dbo.[Product] ([SubcategoryId], [Name], [Description], [Price], [Quantity], [Image])
+	VALUES (@StreetId, 'Street 1', 'Street 1 description', 30000.00, 10, 'images/street1.jpg');
+
+	INSERT INTO dbo.[Product] ([SubcategoryId], [Name], [Description], [Price], [Quantity], [Image])
+	VALUES (@StreetId, 'Street 2', 'Street 2 description', 40000.00, 10, 'images/street2.jpg');
+
+	INSERT INTO dbo.[Product] ([SubcategoryId], [Name], [Description], [Price], [Quantity], [Image])
+	VALUES (@TrailId, 'Trail 1', 'Trail 1 description', 50000.00, 10, 'images/trail1.jpg');
+
+	INSERT INTO dbo.[Product] ([SubcategoryId], [Name], [Description], [Price], [Quantity], [Image])
+	VALUES (@TrailId, 'Trail 2', 'Trail 2 description', 60000.00, 10, 'images/trail2.jpg');
+
+	INSERT INTO dbo.[Product] ([SubcategoryId], [Name], [Description], [Price], [Quantity], [Image])
+	VALUES (@EngineId, 'Engine 1', 'Engine 1 description', 70000.00, 10, 'images/engine1.jpg');
+
+	INSERT INTO dbo.[Product] ([SubcategoryId], [Name], [Description], [Price], [Quantity], [Image])
+	VALUES (@EngineId, 'Engine 2', 'Engine 2 description', 80000.00, 10, 'images/engine2.jpg');
+
+	INSERT INTO dbo.[Product] ([SubcategoryId], [Name], [Description], [Price], [Quantity], [Image])
+	VALUES (@ElectronicsId, 'Electronics 1', 'Electronics 1 description', 900.00, 10, 'images/electronics1.jpg');
+
+	INSERT INTO dbo.[Product] ([SubcategoryId], [Name], [Description], [Price], [Quantity], [Image])
+
+	VALUES (@ElectronicsId, 'Electronics 2', 'Electronics 2 description', 100.00, 10, 'images/electronics2.jpg');
+
+	INSERT INTO dbo.[Product] ([SubcategoryId], [Name], [Description], [Price], [Quantity], [Image])
+	VALUES (@AccessoriesId, 'Accessories 1', 'Accessories 1 description', 110.00, 10, 'images/accessories1.jpg');
+
+	INSERT INTO dbo.[Product] ([SubcategoryId], [Name], [Description], [Price], [Quantity], [Image])
+	VALUES (@AccessoriesId, 'Accessories 2', 'Accessories 2 description', 120.00, 10, 'images/accessories2.jpg');
+
+	INSERT INTO dbo.[Product] ([SubcategoryId], [Name], [Description], [Price], [Quantity], [Image])
+	VALUES (@CasualId, 'Casual 1', 'Casual 1 description', 130.00, 10, 'images/casual1.jpg');
+
+	INSERT INTO dbo.[Product] ([SubcategoryId], [Name], [Description], [Price], [Quantity], [Image])
+	VALUES (@CasualId, 'Casual 2', 'Casual 2 description', 140.00, 10, 'images/casual2.jpg');
+
+	INSERT INTO dbo.[Product] ([SubcategoryId], [Name], [Description], [Price], [Quantity], [Image])
+	VALUES (@HelmetsId, 'Helmets 1', 'Helmets 1 description', 150.00, 10, 'images/helmets1.jpg');
+
+	INSERT INTO dbo.[Product] ([SubcategoryId], [Name], [Description], [Price], [Quantity], [Image])
+	VALUES (@HelmetsId, 'Helmets 2', 'Helmets 2 description', 160.00, 10, 'images/helmets2.jpg');
+
+	INSERT INTO dbo.[Product] ([SubcategoryId], [Name], [Description], [Price], [Quantity], [Image])
+	VALUES (@GlovesId, 'Gloves 1', 'Gloves 1 description', 170.00, 10, 'images/gloves1.jpg');
+
+	INSERT INTO dbo.[Product] ([SubcategoryId], [Name], [Description], [Price], [Quantity], [Image])
+	VALUES (@GlovesId, 'Gloves 2', 'Gloves 2 description', 180.00, 10, 'images/gloves2.jpg');
+
+	INSERT INTO dbo.[Product] ([SubcategoryId], [Name], [Description], [Price], [Quantity], [Image])
+	VALUES (@BootsId, 'Boots 1', 'Boots 1 description', 190.00, 10, 'images/boots1.jpg');
+
+	INSERT INTO dbo.[Product] ([SubcategoryId], [Name], [Description], [Price], [Quantity], [Image])
+	VALUES (@BootsId, 'Boots 2', 'Boots 2 description', 200.00, 10, 'images/boots2.jpg');
+
+END
---
+++ Jotocca.Database/dbo/Views/ProductWithSubcategory.sql
@@ -0,0 +1,12 @@
+﻿CREATE VIEW [dbo].[ProductWithSubcategory]
+AS SELECT
+    S.[Name] AS SubcategoryName,
+    P.[Name] AS ProductName,
+    P.[Description] AS ProductDescription,
+    P.[Price] AS ProductPrice,
+    P.[Quantity] AS ProductQuantity,
+    P.[Image] AS ProductImage
+FROM
+    dbo.[Product] P
+INNER JOIN
+    dbo.[Subcategory] S ON S.[SubcategoryId] = P.[SubcategoryId]
