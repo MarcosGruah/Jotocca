c3f43dd3dc53e0b5fd2da6896a2115186e32e32a
MarcosGruah <110102063+MarcosGruah@users.noreply.github.com>
2024-01-26 23:51:49 -0300

    Subcategory Table Post Deployment seeding

--- Jotocca.Database/dbo/Script.PostDeployment.sql
+++ Jotocca.Database/dbo/Script.PostDeployment.sql
@@ -27,3 +27,40 @@ BEGIN
     INSERT INTO dbo.Category ([Name], [Description])
     VALUES('Apparel & Gear', 'Fashionable clothing and protective gear for all occasions');
 END
+
+IF NOT EXISTS (SELECT 1 FROM dbo.[Subcategory])
+BEGIN
+	DECLARE @MotorcyclesId UNIQUEIDENTIFIER = (SELECT [CategoryId] FROM dbo.Category WHERE [Name] = 'Motorcycles');
+	DECLARE @PartsAndAccessoriesId UNIQUEIDENTIFIER = (SELECT [CategoryId] FROM dbo.Category WHERE [Name] = 'Parts & Accessories');
+	DECLARE @ApparelAndGearId UNIQUEIDENTIFIER = (SELECT [CategoryId] FROM dbo.Category WHERE [Name] = 'Apparel & Gear');
+
+	INSERT INTO dbo.[Subcategory] ([CategoryId], [Name], [Description])
+	VALUES (@MotorcyclesId, 'Scooter', 'Compact and efficient urban commuting');
+
+	INSERT INTO dbo.[Subcategory] ([CategoryId], [Name], [Description])
+	VALUES (@MotorcyclesId, 'Street', 'Versatile for city and highway riding');
+
+	INSERT INTO dbo.[Subcategory] ([CategoryId], [Name], [Description])
+	VALUES (@MotorcyclesId, 'Trail', 'Designed for off-road and trail adventures');
+
+	INSERT INTO dbo.[Subcategory] ([CategoryId], [Name], [Description])
+	VALUES (@PartsAndAccessoriesId, 'Engine', 'Engine parts and components');
+
+	INSERT INTO dbo.[Subcategory] ([CategoryId], [Name], [Description])
+	VALUES (@PartsAndAccessoriesId, 'Electronics', 'Electronic components and accessories');
+
+	INSERT INTO dbo.[Subcategory] ([CategoryId], [Name], [Description])
+	VALUES (@PartsAndAccessoriesId, 'Accessories', 'Accessories for motorcycles');
+
+	INSERT INTO dbo.[Subcategory] ([CategoryId], [Name], [Description])
+	VALUES (@ApparelAndGearId, 'Casual', 'Casual clothing and accessories');
+
+	INSERT INTO dbo.[Subcategory] ([CategoryId], [Name], [Description])
+	VALUES (@ApparelAndGearId, 'Helmets', 'Protective helmets');
+
+	INSERT INTO dbo.[Subcategory] ([CategoryId], [Name], [Description])
+	VALUES (@ApparelAndGearId, 'Gloves', 'Protective gloves');
+
+	INSERT INTO dbo.[Subcategory] ([CategoryId], [Name], [Description])
+	VALUES (@ApparelAndGearId, 'Boots', 'Protective boots');
+END
