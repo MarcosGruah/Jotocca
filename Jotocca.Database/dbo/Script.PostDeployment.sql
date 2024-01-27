f6f7e38e51be1603da69162ec2e5dde65ef1c4d5
MarcosGruah <110102063+MarcosGruah@users.noreply.github.com>
2024-01-26 23:34:40 -0300

    Category Table Post Deployment seeding

--- Jotocca.Database/dbo/Script.PostDeployment.sql
+++ Jotocca.Database/dbo/Script.PostDeployment.sql
@@ -2,12 +2,28 @@
 BEGIN
 	INSERT INTO dbo.[User] ([Name])
 	VALUES ('Emma Johnson');
+
 	INSERT INTO dbo.[User] ([Name])
 	VALUES ('Samuel Martinez');
+
 	INSERT INTO dbo.[User] ([Name])
 	VALUES ('Olivia Thompson');
+
 	INSERT INTO dbo.[User] ([Name])
 	VALUES ('Ethan Rodriguez');
+
 	INSERT INTO dbo.[User] ([Name])
 	VALUES ('Isabella Santos');
+END
+
+IF NOT EXISTS (SELECT 1 FROM dbo.[Category])
+BEGIN
+    INSERT INTO dbo.Category ([Name], [Description])
+    VALUES('Motorcycles', 'New or used motorcycles');
+
+    INSERT INTO dbo.Category ([Name], [Description])
+    VALUES('Parts & Accessories', 'Engine parts, electronic components, and accessories');
+
+    INSERT INTO dbo.Category ([Name], [Description])
+    VALUES('Apparel & Gear', 'Fashionable clothing and protective gear for all occasions');
 END
